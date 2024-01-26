3333f0d22c427ec522d947bf75b1de46362e1d8a
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-01-26 10:37:13 +0200

    Add dcostring and version to the module

--- google_takeout_email.py
+++ google_takeout_email.py
@@ -1,5 +1,19 @@
+'''
+Contains a reader for mailbox files, MailboxReader, and utility functions for parsing
+email addresses. Read from the mailbox.
+
+The MailboxReader class functions as a context manager that opens a mailbox file. The
+messages property returns an iterator of email.message objects.
+
+The strip_address function returns the first actual email address contained in a string
+and the parse_address_list returns a list of addresses in a comma separated list.
+
+'''
+
 import email
 import re
+
+__version__ = "0.0.1"
 
 
 class MailboxReader():
